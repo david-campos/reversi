da5490325de1cfb9b7987efae63a14283d13d1d6
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2017-03-17 17:31:05 +0100

    Created a "no_interface.html" with the code I had prepared for NodeJS to run the code faster at least

--- node/generate.py
+++ node/generate.py
@@ -2,7 +2,7 @@
 
 import os
 
-filenames = ['../chrome/neural_network.js', '../chrome/reversi_ai.js', 'game.js']
+filenames = ['../chrome/neural_network.js', '../chrome/reversi_ai.js', '../chrome/game_no_interface.js']
 try:
     os.remove('generated.js')
 except:
